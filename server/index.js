3dd5bb814f6e2207764049db7bbeddace4732713
Jyotideepjee1803 <jdp_jan@outlook.com>
2024-08-08 19:40:20 +0530

    added upload functionality

--- server/index.js
+++ server/index.js
@@ -1,37 +1,36 @@
 // server/server.js
+require('dotenv').config();
 const express = require('express');
 const multer = require('multer');
+const { PutObjectCommand } = require('@aws-sdk/client-s3');
+const { memoryStorage } = require('multer')
+const storage = memoryStorage()
+const upload = multer({ storage })
 const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const app = express();
-const corsOptions = {
-    origin: 'http://localhost:5174',
-    credentials: true,
-    optionsSuccessStatus: 200,
-};
-
-app.use(express.json());
-app.use(cors(corsOptions));
-
-const upload = multer({ dest: 'uploads/' });
-
-
+const { s3_bucket, s3 } = require('./config/s3');
+const { connectDB } = require('./config/db');
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://ayushmannayak2311:ayushmannayak2311@cluster0.tlzly8z.mongodb.net/dsp?retryWrites=true&w=majority&appName=Cluster0', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((err) => {
-    console.error("Failed to connect to MongoDB", err);
-    process.exit(1); // Exit the process with failure
-});
-// Schema for file metadata// Schema for file metadata
+connectDB();
 
-const fileSchema = new mongoose.Schema({
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+const app = express();
+const DIRNAME = path.resolve();
+const PORT = process.env.PORT || 5000;
+
+//Middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Schema for file metadata// Schema for file metadata
+  const fileSchema = new mongoose.Schema({
     originalName: String,
     encoding: String,
     mimeType: String,
@@ -43,27 +42,64 @@ const fileSchema = new mongoose.Schema({
   const File = mongoose.model('File', fileSchema);
   
   // Endpoint to handle file upload
-  app.post('/api/upload', upload.single('file'), async (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
-    }
+  // app.post('/api/upload', upload.single('file'), async (req, res) => {
+  //   if (!req.file) {
+  //     return res.status(400).json({ error: 'No file uploaded' });
+  //   }
   
-    const file = req.file;
-    const fileMetadata = new File({
-      originalName: file.originalname,
-      encoding: file.encoding,
-      mimeType: file.mimetype,
-      size: file.size,
-      path: file.path,
-    });
+  //   const file = req.file;
+  //   const fileMetadata = new File({
+  //     originalName: file.originalname,
+  //     encoding: file.encoding,
+  //     mimeType: file.mimetype,
+  //     size: file.size,
+  //     path: file.path,
+  //   });
   
+  //   try {
+  //     await fileMetadata.save();
+  //     res.json({ message: 'File uploaded successfully', file: fileMetadata });
+  //   } catch (error) {
+  //     res.status(500).json({ error: 'Failed to save file metadata' });
+  //   }
+  // });
+
+  const generateUniqueFileName = (file) => {
+    return (
+      (Date.now() + Math.random()).toString().replace(".", "") +
+      "---" +
+      file.originalname
+    );
+  };
+
+  const uploadAudio = async (filename, file) => {
+      const params = {
+          Key: filename,
+          Bucket: s3_bucket,
+          Body: file,
+          ContentType: 'audio/mpeg',
+          ACL: 'public-read',
+      };
+
+      try {
+          const command = new PutObjectCommand(params);
+          const data = await s3.send(command);
+          const location = `https://${s3_bucket}.s3.${process.env.S3_REGION}.amazonaws.com/${filename}`;
+          return location;
+      } catch (err) {
+          throw err;
+      }
+  };
+  
+  app.post('/upload', upload.single('audiofile'), async (req, res) => {
     try {
-      await fileMetadata.save();
-      res.json({ message: 'File uploaded successfully', file: fileMetadata });
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to save file metadata' });
+      const filename = generateUniqueFileName(req.file);
+      const file = req.file.buffer;
+      const link = await uploadAudio(filename, file);
+      res.status(200).json({signed_url : link});
+    }catch(err) {
+      res.status(500).json({ message: 'Failed to upload audio file', error: err.message });
     }
   });
-  
-  const PORT = process.env.PORT || 5000;
+
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
